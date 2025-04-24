8281396ac7653b795f56514661d243b1f8686dba
fabricemoreau <f.moreau@arvalis.fr>
2025-04-24 10:17:34 +0200

    search_model

---
+++ src/models/search_model.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+import joblib
+import numpy as np
+
+print(joblib.__version__)
+
+PROCESSED_DATA_PATH = "data/processed"
+
+X_train = pd.read_csv(PROCESSED_DATA_PATH + '/X_train_scaled.csv')
+X_test = pd.read_csv(PROCESSED_DATA_PATH + '/X_test_scaled.csv')
+y_train = pd.read_csv(PROCESSED_DATA_PATH + '/y_train.csv')
+y_test = pd.read_csv(PROCESSED_DATA_PATH + '/y_test.csv')
+y_train = np.ravel(y_train)
+y_test = np.ravel(y_test)
+
+param_rf = {
+    'n_estimators': [50, 100, 200, 400], 
+    'max_depth': [None, 5, 10, 15],
+    'max_features': range(1, X_train.shape[1])
+}
+
+grid_search = GridSearchCV(estimator=RandomForestRegressor(n_jobs = -1, random_state = 42), 
+                           param_grid=param_rf, 
+                           cv=3, 
+                           n_jobs = -1, 
+                           verbose = 1)
+grid_search.fit(X_train, y_train)
+
+#--Save the trained model to a file
+model_filename = './models/best_model.pkl'
+joblib.dump(grid_search.best_estimator_, model_filename)
+print("Model trained and saved successfully.")
